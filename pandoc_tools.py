dd47907412a2b25e32dfb14f36f447a78b21d42d
AvianY <ayelite@gmail.com>
2022-10-15 12:37:15 +0200

    CREATE_NO_WINDOW only on windows

--- pandoc_tools.py
+++ pandoc_tools.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import os
+import os, sys
 
 from typing import List
 import subprocess
@@ -17,13 +17,16 @@ from printers.json_list_media import json_get_media
 from filters.prepare_for_dokuwiki import json_prepare_for_dokuwiki
 
 
-def script_dir():
-    return Path(os.path.dirname(os.path.abspath(__file__)))
-
-
+def subprocess_run(args, input=None, cwd=os.path.dirname(os.path.abspath(__file__)), capture_output=True):
+    if sys.platform == 'win32':
+        CREATE_NO_WINDOW = 0x08000000
+        p = subprocess.run(args, capture_output=True, input=input, cwd=cwd, creationflags=CREATE_NO_WINDOW)
+    else:
+        p = subprocess.run(args, capture_output=True, input=input, cwd=cwd)
+    return p
+    
 def zim_filepath_to_json(filepath, filtered=True):
-    CREATE_NO_WINDOW = 0x08000000
-    p = subprocess.run([shutil.which('pandoc'), '-f', 'zimwiki_reader.lua', '-t', 'json', str(filepath)], capture_output=True, cwd=str(script_dir()), creationflags=CREATE_NO_WINDOW)
+    p = subprocess_run([shutil.which('pandoc'), '-f', 'zimwiki_reader.lua', '-t', 'json', str(filepath)])
     json_file = json.loads(p.stdout.decode())
     if filtered:
         json_file = json_transform_rawblocks_to_codeblocks(json_file)
@@ -47,8 +50,7 @@ def get_links_from_zim_filepath(filepath, zim_pages_only):
 
 def create_pdf_from_json(json_dict: dict, target_file, pdf_options, notebook_folder) -> None:
     json_file: str = json.dumps(json_dict)
-    CREATE_NO_WINDOW = 0x08000000
-    p = subprocess.run(['pandoc', '-f', 'json', '-t', 'pdf', '--pdf-engine=xelatex', '-o', target_file] + pdf_options, input=json_file.encode(), cwd=notebook_folder, creationflags=CREATE_NO_WINDOW)
+    p = subprocess_run(['pandoc', '-f', 'json', '-t', 'pdf', '--pdf-engine=xelatex', '-o', target_file] + pdf_options, input=json_file.encode(), cwd=notebook_folder)
     p.check_returncode()
 
 def get_media_from_json(json_input: dict) -> List[str]:
@@ -61,7 +63,6 @@ def get_rawtexts_from_json(json_input: dict) -> List[str]:
 def json_to_dokuwiki(json_input, prefix) -> str:
     # does not work on raw parsed zimwiki
     prepared_json = json.dumps(json_prepare_for_dokuwiki(json_input, prefix=prefix))
-    CREATE_NO_WINDOW = 0x08000000
-    p = subprocess.run([shutil.which('pandoc'), '-f', 'json', '-t', 'dokuwiki'], input=prepared_json.encode(), capture_output=True, cwd=str(script_dir()), creationflags=CREATE_NO_WINDOW)
+    p = subprocess_run([shutil.which('pandoc'), '-f', 'json', '-t', 'dokuwiki'], input=prepared_json.encode())
     return p.stdout.decode()
 
